127e7826ba49d1231b29870d5e17b77f1f305624
Juan Wagner <juanswagner@gmail.com>
2021-04-18 13:27:41 -0300

    Clase 25

--- src/config/session.ts
+++ src/config/session.ts
@@ -1,9 +1,21 @@
 import { SessionOptions } from "express-session";
+import connectRedis from "connect-redis";
+import session from "express-session";
+import redis from "redis";
+
+const redisClient = redis.createClient();
+const RedisStorage = connectRedis(session);
 
 export const sessionConfig: SessionOptions = {
+  store: new RedisStorage({
+    host: "localhost",
+    port: 6379,
+    client: redisClient,
+    ttl: 300,
+  }),
   secret: process.env.SESSION_SECRET || "my-super-secrettASKLOjufdlaksjdklsaj",
   resave: true,
   saveUninitialized: true,
-  rolling:true,
+  rolling: true,
   cookie: { maxAge: Number(process.env.SESSION_COOKIE_MAXAGE) || 60 },
 };
